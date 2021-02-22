6a4cca08417f3dee26271fae5eb0003622b12899
Renzo <rmolivares@renzo-olivares.dev>
2021-02-22 00:01:10 -0800

    Update test cases expected output

--- examples/test35.js
+++ examples/test35.js
@@ -19,9 +19,13 @@ arrayOfFunction[y]();
 
 
 /*Expected Output
-{ '(test35.js:17:9:17:12)':
-   [ '(test35.js:14:9:16:2)' ],
-  '(test35.js:18:1:18:21)':
-   [ '(test35.js:6:3:8:4)' ],
-  '(test35.js:7:5:7:42)': [ 'bound consoleCall (Native)' ] }
+{
+  '(test35.js:17:9:17:12)': [
+    '(test35.js:14:9:16:2)'
+  ],
+  '(test35.js:18:1:18:21)': [
+    '(test35.js:6:3:8:4)'
+  ],
+  '(test35.js:7:5:7:42)': [ 'log (Native)' ]
+}
 */
--- examples/test88.js
+++ examples/test88.js
@@ -19,10 +19,14 @@ proxy1.eyeCount;
 
 
 /*Expected Output
-{ '(test88.js:14:16:14:29)':
-   [ '(test88.js:1:1:3:2)' ],
+{
+  '(test88.js:14:16:14:29)': [
+    '(test88.js:1:1:3:2)'
+  ],
   '(test88.js:15:14:15:43)': [ 'Proxy (Native)' ],
-  'system (Native)':
-   [ '(test88.js:6:8:12:4)' ],
-  '(test88.js:8:7:8:63)': [ 'bound consoleCall (Native)' ] }
+  'system (Native)': [
+    '(test88.js:6:8:12:4)'
+  ],
+  '(test88.js:8:7:8:63)': [ 'log (Native)' ]
+}
 */
