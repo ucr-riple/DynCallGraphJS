c5f899d31838f444010445d2e4e96520bb53a9f3
Renzo <rmolivares@renzo-olivares.dev>
2021-02-21 22:59:47 -0800

    Use a map to store expected results instead of array * When readdirSync reads the files in a directory it does not do it in order, so if we rely on this order our test cases fail. * Instead let's rely on the test name, by using a map to store the results.

--- test_suite/DCG_tests.test.js
+++ test_suite/DCG_tests.test.js
@@ -5,31 +5,32 @@ const path = require('path')
 
 describe('DCG Test Suite', () => {
     const resultsPath = path.join(__dirname, '/expected')
-    const casesAmount = 2
+    const casesAmount = 99
     var cases = []
-    var expectedResults = []
-    var resultsCounter = 1
+    var expectedResults = new Map()
 
     for(var i = 1; i <= casesAmount; i++){
         cases[i] = i
     }
 
     fs.readdirSync(resultsPath).forEach(element => {
+        let testName = element.replace(".txt", "")
         let fullPath = path.join(resultsPath, element)
         var results = fs.readFileSync(fullPath).toString()
-        expectedResults[resultsCounter] = results
-        resultsCounter += 1
+        expectedResults[testName] = results
     })
 
     test.each(cases)('test', async (i) => {
         const dcg = await executeDCG(`test${i}.js`)
+        // console.log(dcg)
         strDCG = dcg['stdout'].toString()
+        strDCG = strDCG.replace(/\/\home\/renzo\/research\/DynCallGraphJS\/examples\//g, "")
         strDCG = strDCG.replace(/\s/g, "")
     
-        expect(strDCG).toBe(expectedResults[i].replace(/\s/g, ""))
+        expect(strDCG).toBe(expectedResults[`test${i}`].replace(/\s/g, ""))
     })
 })
 
 async function executeDCG(testFile){
-    return await exec(`node node_modules/jalangi2/src/js/commands/jalangi.js --inlineIID --inlineSource --analysis DCG.js examples/${testFile}`)
+    return await exec(`nodejs node_modules/jalangi2/src/js/commands/jalangi.js --inlineIID --inlineSource --analysis DCG.js examples/${testFile}`)
 }
