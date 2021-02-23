b06ad2308e14b90681d002b6397c79aab3c78fea
Renzo <rmolivares@renzo-olivares.dev>
2021-02-23 11:56:09 -0800

    Make casesAmount dynamic

--- test_suite/DCG_tests.test.js
+++ test_suite/DCG_tests.test.js
@@ -7,20 +7,21 @@ describe('DCG Test Suite', () => {
     const resultsPath = path.join(__dirname, '/expected')
     const excludePath = __dirname.replace('test_suite','examples/')
     const pathRe = new RegExp(`${excludePath}`, 'g')
-    const casesAmount = 99
+    var casesAmount = 0
     var cases = []
     var expectedResults = new Map()
-
-    for(var i = 1; i <= casesAmount; i++){
-        cases[i] = i
-    }
 
     fs.readdirSync(resultsPath).forEach(element => {
         let testName = element.replace(".txt", "")
         let fullPath = path.join(resultsPath, element)
         var results = fs.readFileSync(fullPath).toString()
         expectedResults[testName] = results
+        casesAmount += 1
     })
+
+    for(var i = 1; i <= casesAmount; i++){
+        cases[i] = i
+    }
 
     test.each(cases)('test', async (i) => {
         const dcg = await executeDCG(`test${i}.js`)
